f6f2c431f31f88eb5400c5524e8b5e4f89485cb6
liudi <liudi@gwm.cn>
2026-04-24 13:36:50 +0800

    chore: bootstrap scene hall mvp skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <view class="content">
+    <text class="title">Image Play</text>
+  </view>
+</template>
+
+<style>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.title {
+  font-size: 32px;
+  font-weight: bold;
+}
+</style>
